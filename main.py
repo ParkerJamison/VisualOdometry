b492648f65c6b3460fdc235343c74346e5d8bd76
ParkerJamison <parkerjamison08@gmail.com>
2024-05-19 20:26:46 -0700

    Monocular Odometry

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
+"""
+    Monocular Odometry ----- Parker Jamison 2024
+
+    Calculates the 'Trajectory' of a camera using
+    FAST feature detector, and the Lucas-Kanade
+    feature tracker.
+
+    Using IPhone 14 Pro main camera for camera
+    specifications.
+"""
 import cv2
 import numpy as np
 from odometry import MonoCam
 
-lk_params = dict(winSize=(21, 21),
-                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
-
 
 def main():
 
-    cap = cv2.VideoCapture('IMG_2644.MOV')
-    #cap = cv2.VideoCapture('IMG_2645.MOV')
-    #cap = cv2.VideoCapture(0)
+    # Insert a video here
+    cap = cv2.VideoCapture('IMG_2653.MOV')
+
+    # Initialize the trajectory plot
     traj = np.zeros(shape=(600, 600, 3))
+    cv2.imshow("trajectory", traj)
 
     ret, prev = cap.read()
     image = None
 
-    while True:
+    while cap.isOpened():
         ret, frame = cap.read()
 
+        if not ret:
+            input("press enter to exit: ")
+            break
         # on the first iteration, initialize the monocam object
         if image is None:
-            image = MonoCam(frame, prev, lk_params)
+            image = MonoCam(frame, prev)
             continue
 
         # update the frames
@@ -30,17 +42,21 @@ def main():
 
         image.odometry()
 
-
-        mono = image.monocoordinate()
-        #print(mono)
+        mono = image.multDiagonol()
         draw_x, draw_y, draw_z = [int(round(x)) for x in mono]
-        traj = cv2.circle(traj, (draw_x+300, draw_z+300), 1, list((0, 255, 0)), 1)
+        # multiply the coordinates by two in smaller scale situations to see the trajectory better
+        draw_z <<= 2
+        draw_x <<= 2
+
+        traj = cv2.circle(traj, (-draw_x+300, draw_z+300), 1, list((0, 255, 0)), 1)
 
         cv2.imshow('webcam', image.frame)
         cv2.imshow('trajectory', traj)
 
         if cv2.waitKey(1) == ord('q'):
             break
+    cap.release()
+
 
 if __name__ == '__main__':
     main()
--- odometry.py
+++ odometry.py
@@ -3,99 +3,115 @@ import numpy as np
 
 
 class MonoCam:
-    def __init__(self,
-                 frame,
-                 prev,
-                 lk_params=None,
-                 focal_length=900,
-                 principal_point=(400, 600),
-                 detector=cv2.FastFeatureDetector.create(nonmaxSuppression=True)
-                 ):
-
-        self.prevPoints = None
-        if lk_params is None:
-            lk_params=dict(winSize=(21,21), criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
+    def __init__(self, frame, prev,):
         self.frame = frame
         self.prev = prev
-        self.focal_length = focal_length
-        self.principal_point = principal_point
-        self.coordinate = np.array([[0], [0], [0]])
-        self.lk_params = lk_params
-        self.detector = detector
-        self.R = np.zeros(shape=(3, 3))
-        self.t = np.zeros(shape=(3, 3))
-        self.id = 0
+
+        self.lk_params = dict(winSize=(21, 21), criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
+        self.detector = cv2.FastFeatureDetector.create(threshold=10, nonmaxSuppression=True)
+
+        # Camera specifications
+        self.focal_length = 400
+        self.principal_point = (500, 1000)
+
+        self.R = np.zeros(shape=(3, 3))  # Rotational Matrix
+        self.t = np.zeros(shape=(3, 3))  # Translational Vector
+
+        self.prevPoints = None
+        self.firstRun = True
         self.n_features = 0
 
     def odometry(self):
+        """
+        Main method for odometry,
+        detects keypoints with the FAST feature detector,
+        tracks the keypoints with Lucas-Kanade feature tracker
+        updates the rotational matrix and translational vector
+        """
         # get the current and previous frame ready for detecting
         curr_gray = self.setFrame(self.frame)
         prev_gray = self.setFrame(self.prev)
 
-        # if the number of features gets below 500, recall detect
-        if self.n_features < 2000:
+        # if the number of features gets below 500, re-call detect
+        if self.n_features < 500:
             self.prevPoints = self.getDetector(prev_gray)
 
-        # Lukas Kanade optical flow
+        # Lucas Kanade optical flow
         points, st, err = cv2.calcOpticalFlowPyrLK(prev_gray, curr_gray, self.prevPoints, None, **self.lk_params)
 
-
-        # filter out the best points in Lukas Kanade
+        # filter out the best points in Lucas Kanade
         good_old = self.prevPoints[st == 1]
         good_new = points[st == 1]
 
-        # cv2.undistortPoints(good_new, self.cameraMatrix, dst=good_new)
-
-        if self.id < 2:
+        # We need to initialize the rotation and translation vectors the first time through,
+        if self.firstRun:
             E, _ = cv2.findEssentialMat(good_new, good_old, focal=self.focal_length, pp=self.principal_point,
                                         method=cv2.RANSAC, prob=0.999, threshold=1.0)
 
             _, self.R, self.t, _ = cv2.recoverPose(E, good_old, good_new, focal=self.focal_length,
                                                    pp=self.principal_point)
-            self.id =2
+            self.firstRun = False
+
         else:
             E, _ = cv2.findEssentialMat(good_new, good_old, focal=self.focal_length, pp=self.principal_point,
                                         method=cv2.RANSAC, prob=0.999, threshold=1.0)
-            _, R, t, _ = cv2.recoverPose(E, good_old, good_new, self.R.copy(), self.t.copy(), focal=self.focal_length,
+            _, R, t, _ = cv2.recoverPose(E, good_old, good_new, focal=self.focal_length,
                                          pp=self.principal_point)
 
+            # get the absolute scale of the motion, use it to update the Rotational matrix and translation vector
             absolute_scale = self.getAbsoluteScale(good_old, good_new)
 
-            if (absolute_scale > 0.1 and abs(t[2][0]) > abs(t[0][0]) and abs(t[2][0]) > abs(t[1][0])):
+            if (absolute_scale > 0.1 and (t[2][0]) > (t[0][0]) and (t[2][0]) > (t[1][0])):
                 self.R = R.dot(self.R)
                 self.t = self.t + absolute_scale * self.R.dot(t)
 
-
-        '''
-        for i, (new, old) in enumerate(zip(good_new, good_old)):
-            a, b = new.ravel()
-            a, b= int(a), int(b)
-
-            self.frame = cv2.circle(self.frame, (a, b), 5, (0, 0, 220), -1)
-        '''
-
+        # update the number of features
         self.n_features = good_new.shape[0]
 
     def getAbsoluteScale(self, good_old, good_new):
+        """
+        Calculates the 'scale' of the motion.
+        inputs:
+            good_old ------>  the points calculated through calcopticalflowpyrlk from the prev frame
+            good_new ------>  the points found by calcopticalflowpyrlk
+        output:
+            the absolute scale between 0 and 1
+        """
+        distance = np.linalg.norm(good_new - good_old, axis=1)
+        median = np.median(distance)
 
-        distance = np.linalg.norm(good_new - good_old, axis = 1)
-        return np.log(np.median(distance))
-
+        return np.divide((median % 8), 8)
 
     def getDetector(self, frame):
+        """
+        Detects points in the frame using the FAST detector.
+        input:
+            frame ------> the frame to be detected
+        output:
+            an array of keypoints detected by FAST
+        """
         self.prevPoints = self.detector.detect(frame, None)
-        return np.array([x.pt for x in self.prevPoints], dtype=np.float32).reshape(-1, 1, 2)
+        return np.array([x.pt for x in self.prevPoints], dtype=np.float32).reshape((-1, 1, 2))
 
     def setFrame(self, frame):
+        """
+        Modifies the frame to aid in detecting keypoints.
+        input:
+            frame ------> the frame to be modified
+        output:
+            the color shifted frame
+        """
         return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    def monocoordinate(self):
-        diagnol = np.array([[-1, 0, 0],
+    def multDiagonol(self):
+        """
+        Multiply the translation vector by the diagonol matrix to get the correct orientation
+
+        output:
+            the one dimensional product of the diagonol matrix and the translation vector
+        """
+        diagonol = np.array([[-1, 0, 0],
                            [0, -1, 0],
                            [0, 0, -1]])
-        coord = np.matmul(diagnol, self.t)
+        coord = np.matmul(diagonol, self.t)
         return coord.flatten()
-
-    #def triangulate(self):
-        #todo
-
